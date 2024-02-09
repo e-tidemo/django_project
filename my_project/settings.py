367be35ec6fd6cf2a22397ade3b90b5badaa4d60
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-09 09:54:32 +0100

    Redo everything to sort out the my_project directory and add an about view

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wv5^@ntzj*k_04#m7qizhcstzjic4=!!b$+g+5z^-wl@))e1va'
+SECRET_KEY = 'django-insecure-+-jpthed58hzs4^c#!y$n82#=y0e4igjf4+o6s6g-5r($u===h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
